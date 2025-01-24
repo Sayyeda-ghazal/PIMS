43434d8c40d068d45edb88f20100c131f1f42344
Syeda Ghazal <99088845+Sayyeda-ghazal@users.noreply.github.com>
2025-01-24 15:49:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-import Users_auth, PIMS_auth
+import Users_auth, PIMS_auth, Inventory, export_inventory
 from database import Base, engine
 
 app = FastAPI()
 
 app.include_router(Users_auth.router)  
 app.include_router(PIMS_auth.router)
+app.include_router(Inventory.router)
+app.include_router(export_inventory.router)
 
 Base.metadata.create_all(engine)
